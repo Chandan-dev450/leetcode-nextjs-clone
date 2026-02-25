2884f88f89bf50b043a18d349431b1f4c2dc1fb7
Chandan <chandushetty464@gmail.com>
2026-02-25 22:44:13 +0530

    init

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <TooltipProvider>
         {children}
+      </TooltipProvider>
       </body>
     </html>
   );
