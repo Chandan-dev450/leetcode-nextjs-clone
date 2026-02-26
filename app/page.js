cf96ac20c0ac454a21c554e44545c48bbf928ac0
Chandan <chandushetty464@gmail.com>
2026-02-26 19:57:02 +0530

    Impelmented clerk authentication

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
-export default function Home() {
+
+import { onBoardUser } from "@/modules/auth/actions";
+import { UserButton } from "@clerk/nextjs";
+
+export default async function Home() {
+  await onBoardUser();
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Button>Click me!</Button>
+      <UserButton/>
     </div>
   );
 }
